cc03d47854f86bc59b9d8cab8dd629113d76f094
deniscozma2019 <denis.cozma2015@gmail.com>
2024-01-29 11:05:29 +0200

    Initial commit

---
+++ src/test/java/com/example/RestaurantManagementSystem/MasterThesis/MasterThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantManagementSystem.MasterThesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
